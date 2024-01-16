bf655fdecebd575c7312d0f17d15d90fa036ced6
csm031 <csm031@naver.com>
2024-01-16 11:38:42 +0900

    test 2

--- test002/src/module-info.java
+++ test002/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module test002 {
+	requires java.sql;
 }
--- test002/src/test002/test003.java
+++ test002/src/test002/test003.java
@@ -1,10 +1,26 @@
 package test002;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class test003 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "c##csm";
+		String password = "csm";
+		try {
+			//driver 로딩
+			Class.forName(driver);
+			System.out.println("jdbc driver 로딩 성공");
+			//DB와 연결
+			DriverManager.getConnection(url, user, password);
+			System.out.println("오라클 연결 성공");
+		} catch (ClassNotFoundException e) {
+			System.out.println("jdbc driver 로딩 실패");
+		} catch (SQLException e) {
+			System.out.println("오라클 연결 실패");
+		}
 	}
-
 }
